3eab0c30673806dace7c5282937ce998e696fa35
Eder Dias <edderdias@gmail.com>
2022-09-29 16:34:07 -0300

    OneSignal inclusão do scope 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,9 +17,9 @@ export class AppComponent implements OnInit {
         this.oneSignal.init({
             appId: "8c39c189-54b0-4160-82c9-b9e7e4e2e0cb",
             serviceWorkerParam: {
-                scope: "/OneSignalSDKWorker.js"
+                scope: "1/OneSignalSDKWorker.js"
               },
-              serviceWorkerPath: 'app.proextrato.com.br/OneSignalSDKWorker.js'
+              serviceWorkerPath: '2app.proextrato.com.br/OneSignalSDKWorker.js'
           });
     }
 
