841e6beac10a89c46600447dec0da8f1472b838e
Eder Dias <edderdias@gmail.com>
2023-04-25 14:21:50 -0300

    Ajustes na consulta de empresa

--- src/app/pages/dashboard/dashboard-customer/dashboard-customer.component.ts
+++ src/app/pages/dashboard/dashboard-customer/dashboard-customer.component.ts
@@ -159,7 +159,7 @@ export class DashboardCustomerComponent implements OnInit {
             // v['value'].map(v => {
             //     this.loadAccounts(v.Id)
             // })
-            this.loadAccounts(v['value'].Id)
+            // this.loadAccounts(v['value'].Id)
         }, e => {
             this.messageService.add(Util.pushErrorMsg(e))
         }).add(() => this.dadosDefault.exibirLoader.next(false))
--- src/app/pages/not-found/app.notfound.component.ts
+++ src/app/pages/not-found/app.notfound.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notfound',
   templateUrl: './app.notfound.component.html',
 })
 export class AppNotfoundComponent {
+
+  public counter = ''
+
+  constructor(private router: Router){}
+  
+  home(){
+    this.counter = localStorage.getItem('counter')
+    if(this.counter === 'false'){
+      this.router.navigate(['dashboard-company'])
+    } else {
+      this.router.navigate(['dashboard-home'])
+    }
+    // [routerLink]="['/home']"
+  }
 
 }
